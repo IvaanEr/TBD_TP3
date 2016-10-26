ca1b21bc01b3bb9246ce6dd329f044e4bc836b7e
IvaanEr <ivan_ernandorena@hotmail.com>
2016-10-26 10:00:53 -0300

    With comments

--- ej5.sql
+++ ej5.sql
@@ -32,3 +32,9 @@ CREATE OR REPLACE VIEW Ci (codigo_cliente, codigo) AS SELECT codigo_cliente, cod
 CREATE OR REPLACE VIEW VisitAll (codigo_cliente) AS SELECT DISTINCT codigo_cliente FROM PrefiereZona WHERE NOT EXISTS (SELECT * FROM Ci WHERE PrefiereZona.codigo_cliente = Ci.codigo_cliente AND NOT EXISTS (SELECT * FROM Visitas WHERE Ci.codigo_cliente = Visitas.codigo_cliente AND Visitas.codigo_inmueble = Ci.codigo));
 
 CREATE OR REPLACE VIEW VAPref (poblacion,zona) AS SELECT nombre_poblacion, nombre_zona from PrefiereZona, VisitAll where VisitAll.codigo_cliente = PrefiereZona.codigo_cliente;
+
+CREATE OR REPLACE VIEW Aux (poblacion,zona,poblacion_2,zona_2) AS (SELECT Zona.nombre_poblacion,Zona.nombre_zona,Limita.nombre_poblacion_2,Limita.nombre_zona_2 FROM Zona,Limita WHERE Zona.nombre_poblacion = Limita.nombre_poblacion AND Zona.nombre_zona = Limita.nombre_zona) UNION (SELECT Zona.nombre_poblacion,Zona.nombre_zona,Limita.nombre_poblacion,Limita.nombre_zona FROM Zona,Limita WHERE Zona.nombre_poblacion = Limita.nombre_poblacion_2 AND Zona.nombre_zona = Limita.nombre_zona_2);
+
+CREATE OR REPLACE VIEW Aux2 (cliente,poblacion,zona) AS (SELECT PrefiereZona.codigo_cliente,nombre_poblacion,nombre_zona FROM PrefiereZona,VisitAll WHERE PrefiereZona.codigo_cliente = VisitAll.codigo_cliente);
+
+SELECT cliente,poblacion_2,zona_2 FROM Aux,Aux2 WHERE Aux.poblacion = Aux2.poblacion AND Aux.zona = Aux2.zona;
